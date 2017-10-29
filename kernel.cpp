1b77e2827a714f15b7339d70d339e8f0e2dd687b
Euab <euanmills26@gmail.com>
2017-10-29 11:34:19 +0000

    Global Descriptor Table

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,6 @@
 
 #include "types.h"
+#include "gdt.h"
 
 void printf(char* str)
 {
@@ -21,6 +22,8 @@ extern "C" void callConstructors()
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t)
 {
 	printf("Hello World!");
+
+	GlobalDescriptorTable gdt;
 
 	while(1);
 }
